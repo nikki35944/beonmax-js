6cf6dd96f3de2b28cd4bce07f745d10c3a1a23a0
nikki35944 <nikki35944@yandex.ru>
2021-12-09 16:57:10 +0300

    first practise

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 "use strict";
 
+var money = prompt('Ваш бюджет на месяц?'),
+    time = prompt('Введите дату в формате YYYY-MM-DD'),
+    requiredExpenes = prompt('Введите обязательную статью расходов в этом месяце'),
+    howMuch = prompt('Во сколько обойдется?');
+var appData = {
+    budget : money,
+    timeData : time,
+    expenses : {
+        requiredExpenes : howMuch,
+    },
+    optionalExpenses : '',
+    income : [],
+    savings : false,
+};
+
+
+alert(money / 30);
