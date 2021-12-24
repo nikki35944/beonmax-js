263a070ad65c29ae5e0e624357c14c8ec12cc79f
nikki35944 <nikki35944@yandex.ru>
2021-12-24 14:32:15 +0300

    timer completed

--- part3/js/script.js
+++ part3/js/script.js
@@ -40,4 +40,53 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+
+//    Timer
+    let deadline = '2021-12-25';
+
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds,
+        };
+    }
+
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+
+            (t.hours.toString().length < 2) ?
+                hours.textContent = '0' + t.hours : hours.textContent = t.hours;
+            (t.minutes.toString().length < 2) ?
+                minutes.textContent = '0' + t.minutes : minutes.textContent = t.minutes;
+            (t.seconds.toString().length < 2) ?
+                seconds.textContent = '0' + t.seconds : seconds.textContent = t.seconds;
+
+
+            if (t.total <= 0 ) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    setClock('timer', deadline);
+
 });
